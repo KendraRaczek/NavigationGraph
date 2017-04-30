6f26b56b34c7fea798781dd5c40186bbfea44987
jnelson33 <jnelson33@wisc.edu>
2017-04-29 21:05:50 -0500

    hjdas

--- NavigationGraph.java
+++ NavigationGraph.java
@@ -28,35 +28,85 @@ public class NavigationGraph implements GraphADT<Location, Path> {
 		
 	}
 
-	public void addEdge(Location src, Location dest, Path edge) {
+	public void addEdge(V src, V dest, E edge)
+	{
+		int numVertices = getNumVertices();
+		if (numVertices > src || numVertices > dest) 
+		{
+			throw  new IndexOutOfBoundsException(); //Not sure which exception to throw
+		}
+		matrix[src][dest] = edge; //Find Agacency matrix
+	}
+	
+	public List<V> getVertices() {
 		
 	}
 	
-	public List<Location> getVertices() {
+	public E getEdgeIfExists(V src, V dest) 
+	{
+		int numVertices = getNumVertices();
+		if (numVertices > src || numVertices > dest)
+		{
+			throw  new IndexOutOfBoundsException(); 
+		}
+		else 
+		{
+			return matrix[src][dest] != 0;
+		}
+	
+	}
+	
+	public List<E> getOutEdges(V src) 
+	{
+	    List list = new ArrayList();
+		int numVertices = getNumVertices();
+		for (int i = 0; i < numVertices; i++)
+		{
+			double temp = matrix[src][i];
+			if (temp != 0) 
+			{
+				list.add(temp); //What is the list?
+			}
+		}
+		return list;
+	}
+	
+	public List<V> getNeighbors(V vertex) {
+//		  int numVertices = getNumVertices();
+//		  if (numV <= v) 
+//		  {
+//			  throw new IndexOutOfBoundsException();
+//		  }
+//		  else
+//		  {
+//			  List<Integer> neighbors = new ArrayList<Integer>();
+//			  for (int x : adjListsMap.get(vertex)) 
+//			  {
+//				  neighbors.add(x);
+//			  }
+//		  }
+//		  
+//		    }
+//		    
+//		    return neighbors;
+//		
+	}
+	
+	public List<E> getShortestRoute(V src, V dest, String edgePropertyName) {
 		
 	}
 	
-	public Path getEdgeIfExists(Location src, Location dest) {
+	public String[] getEdgePropertyNames()
+	{
 		
 	}
 	
-	public List<Path> getOutEdges(Location src) {
+	public int getNumVertices() 
+	{
 		
 	}
-	
-	public List<Location> getNeighbors(Location vertex) {
-		
-	}
-	
-	public List<Path> getShortestRoute(Location src, Location dest, String edgePropertyName) {
-		
-	}
-	
-	public String[] getEdgePropertyNames() {
-		
-	}
-	
-	public String toString() {
+	public String toString() 
+	{
 		
 	}
 	
