70ad10de013fe81675a325162ff1b438a96d6dcc
dzhu46 <mutantburgrz@gmail.com>
2017-05-02 22:17:18 -0500

    Update NavigationGraph.java

--- NavigationGraph.java
+++ NavigationGraph.java
@@ -4,8 +4,8 @@
 // FILE:             NavigationGraph.java
 // TEAM:    72
 // Author: Jonathan Nelson, jnelson33@wisc.edu, jnelson, Lec 001
-// Author: Add
-// Author: Add
+// Author: David Zhu, dzhu46@wisc.edu, zhu, Lec 002
+// Author: 
 // Author: Add
 //
 //////////////////////////// 80 columns wide //////////////////////////////////
